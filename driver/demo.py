3e52ab7e02979b32823ccb9ead37c1946c38f29d
Yuxuan Zhang <zyx@z-yx.cc>
2026-01-31 03:10:54 -0500

    update driver

--- driver/demo.py
+++ driver/demo.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 import argparse
 from time import sleep
-from sys import stdout, stderr
+from math import degrees, radians, sin, cos
+from sys import path, stdout, stderr
+from posixpath import dirname
+
+path.append(dirname(__file__))
 
 from lib.expect import Expect
 from lib.driver import Driver
@@ -20,57 +24,54 @@ parser.add_argument(
     default=0,
     help="Increase verbosity level",
 )
-parser.add_argument(
-    "-p",
-    "--plot",
-    action="store_true",
-    help="Enable plotting of stall guard results",
-)
 args = parser.parse_args()
 baud = int(args.baud)
 verbose = int(args.verbose)
-should_plot = bool(args.plot)
 
 driver = Driver(baudrate=baud, verbose=verbose)
 print(f"Device Info: {driver.getInfo()}", file=stderr)
-m0 = Motor(driver, id=0, invert=1, scale=6.0, init_pos=0.0)
-m1 = Motor(driver, id=1, invert=0, scale=1.0, init_pos=0.0)
-m2 = Motor(driver, id=2, invert=1, scale=0.92, init_pos=0.0)
 
+m0 = Motor(
+    driver,
+    id=0,
+    invert=0,
+    scale=6.0 / 360.0,
+    micro_steps=16,
+    init_pos=0.0,
+    rms_current=1000,
+    stall_sensitivity=50,
+)
 
-@debounce(1.0 / 20.0)
-def tick():
-    if not should_plot:
-        return
-    status = [m.getStatus() for m in (m0, m1, m2)]
-    diag = (f"m{i}.diag={s.diag_pin * 100 - 100}" for i, s in enumerate(status))
-    sg_result = (f"m{i}.sg={s.sg_result}" for i, s in enumerate(status))
-    print("$", *diag, *sg_result)
-    stdout.flush()
+m1 = Motor(
+    driver,
+    id=1,
+    invert=1,
+    scale=6.0 / 360.0,
+    micro_steps=16,
+    init_pos=0.0,
+    rms_current=1000,
+    stall_sensitivity=50,
+)
 
 
 try:
-    with driver.enable(), m0.enable(), m1.enable(), m2.enable():
-        while True:
-            for slot in range(4):
-                Expect.wait(m0.move(+1.0, speed=0.3), m1.move(slot, speed=1.0))
-                for _ in loop_for(0.2):
-                    tick()
-                Expect.wait(m0.move(+0.0, speed=0.3))
-                for _ in loop_for(0.2):
-                    tick()
-                Expect.wait(m0.move(+0.3, speed=0.3))
-                for _ in loop_for(1.0):
-                    tick()
-            for slot in range(4):
-                Expect.wait(m1.move(slot + 4.5, speed=1.0))
-                Expect.wait(m2.move(1.0, speed=2.0))
-                for _ in loop_for(0.2):
-                    tick()
-                Expect.wait(m2.move(0.0, speed=2.0))
-                for _ in loop_for(0.2):
-                    tick()
-            m1.setPosition(m1.position % 6.0)
+    with driver.enable(), m0.enable(), m1.enable():
+        R = 15.0  # degrees
+        V = 15.0  # degrees per second
+        try:
+            Expect.wait(m0.move(R, speed=V), m1.move(0, speed=V))
+            while True:
+                for theta in range(0, 360):
+                    rx = R * cos(radians(theta))
+                    vx = V * max(abs(sin(radians(theta))), 0.1)
+                    ry = R * sin(radians(theta))
+                    vy = V * max(abs(cos(radians(theta))), 0.1)
+                    Expect.wait(
+                        m0.move(rx, speed=vx),
+                        m1.move(ry, speed=vy),
+                    )
+        except KeyboardInterrupt:
+            Expect.wait(m0.move(0.0, speed=V), m1.move(0.0, speed=V))
 except KeyboardInterrupt:
     pass
 
